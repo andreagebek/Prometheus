Updated plotting scripts, minor refactoring for RM-effect and bugfix in sigma_abs calculation

eliteScripts/fluxDecrease.py
@@ -79,9 +79,6 @@ def calculateRM(wavelength, architectureDict, gridsDict):
     T_starrot = architectureDict['period_starrot']
     R_star = architectureDict['R_star']
 
-    phi_steps = gridsDict['phi_steps']
-    rho_steps = gridsDict['rho_steps']
-
     phi_axis = constructAxis(gridsDict, architectureDict, 'phi')
     rho_axis = constructAxis(gridsDict, architectureDict, 'rho')
 
@@ -97,12 +94,7 @@ def calculateRM(wavelength, architectureDict, gridsDict):
     w_star = w_star[SEL]
     F_0 = PHOENIX_output[1][SEL]
 
-    dir_omega = np.array([-np.sin(i_starrot) * np.cos(phi_starrot), -np.sin(i_starrot) * np.sin(phi_starrot), np.cos(i_starrot)])
-    omega = 2. * np.pi / T_starrot * dir_omega # Angular velocity vector of the stellar rotation
-    r_surface = np.array([np.tensordot(np.ones(int(phi_steps)), np.sqrt(R_star**2 - rho_axis**2), axes = 0), 
-    np.tensordot(np.sin(phi_axis), rho_axis, axes = 0), np.tensordot(np.cos(phi_axis), rho_axis, axes = 0)])
-    # Vector to the surface of the star
-    v_los = np.cross(omega, r_surface, axisb = 0)[:, :, 0] # The line-of-sight velocity is the one along the x-axis
+    v_los = geom.calculateStellarLOSvelocity(architectureDict, phi_axis, rho_axis)
     w_shift = gasprop.calculateDopplerShift(v_los)
 
     F_function = interp1d(w_star, F_0, kind = 'cubic')

eliteScripts/gasProperties.py
@@ -252,7 +252,7 @@ def calculateLineAbsorption(wavelength, line_wavelength, line_gamma, line_f, spe
 
     for idx in range(len(line_wavelength)):
 
-        lineProfile = voigt_profile(const.c / wavelength - const.c / line_wavelength[idx], line_gamma[idx], sigma_v / (line_wavelength[idx])) # Calculation in frequency space
+        lineProfile = voigt_profile(const.c / wavelength - const.c / line_wavelength[idx], sigma_v / line_wavelength[idx], line_gamma[idx]) # Calculation in frequency space
         sigma_abs_species += np.pi * (const.e)**2 / (const.m_e * const.c) * line_f[idx] * chi * lineProfile # Draine 2013
 
     return sigma_abs_species

plottingScripts/plotLightcurves.py
@@ -10,6 +10,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 import json
 import sys
+import os
+SCRIPTPATH = os.path.realpath(__file__)
+GITPATH = os.path.dirname(os.path.dirname(SCRIPTPATH))
+PARENTPATH = os.path.dirname(GITPATH)
+sys.path.append(GITPATH)
+import eliteScripts.fluxDecrease as flux 
 
 matplotlib.rcParams['axes.linewidth'] = 2.5
 matplotlib.rcParams['xtick.major.size'] = 10
@@ -29,21 +35,25 @@ Read in settings file and stored light curve
 
 paramsFilename = sys.argv[1]
 
-with open('../../' + paramsFilename + '.txt') as file:
+with open(PARENTPATH + '/setupFiles/' + paramsFilename + '.txt') as file:
     param = json.load(file)
 
-grids_dict = param['Grids']
+architectureDict = param['Architecture']
+gridsDict = param['Grids']
 
-wavelength = np.arange(grids_dict['lower_w'], grids_dict['upper_w'], grids_dict['resolution']) * 1e8 # In Angstrom
+wavelength = flux.constructAxis(gridsDict, architectureDict, 'wavelength')
+orbphase = flux.constructAxis(gridsDict, architectureDict, 'orbphase')
 
-LightcurveFile = np.loadtxt('../../' + paramsFilename + '_lightcurve.txt')
 
-orbphase = LightcurveFile[:, 0]
+LightcurveFile = np.loadtxt(PARENTPATH + '/output/' + paramsFilename + '_lightcurve.txt')
 
-lightcurve_list = []
+R = LightcurveFile[:, 2].reshape(len(wavelength), len(orbphase))
+
+lightcurveList = []
 
 for idx in range(len(wavelength)):
-    lightcurve_list.append(LightcurveFile[:, idx + 1])
+    lightcurveList.append(R[idx, :])
+
 
 """
 Plot the light curve and store the figure
@@ -54,15 +64,16 @@ ax = fig.add_subplot(111)
 
 cmap = matplotlib.cm.get_cmap('Accent')
 
-for idx, lightcurve in enumerate(lightcurve_list):
-    ax.plot(orbphase, lightcurve, color = cmap(float(idx) / (len(wavelength) - 1.)), linewidth = 2, label = str(np.round(wavelength[idx], 2)) + r'$\,\AA$')
+for idx, lightcurve in enumerate(lightcurveList):
+
+    ax.plot(orbphase, lightcurve, color = cmap(float(idx) / (len(wavelength) - 1.)), linewidth = 2, label = str(np.round(wavelength[idx] * 1e8, 2)) + r'$\,\AA$')
 
 lg = ax.legend(loc = 'center right')
 lg.get_frame().set_alpha(0)
 lg.get_frame().set_linewidth(0)
 
 ax.set_xlim(np.min(orbphase), np.max(orbphase))
-ax.set_ylim(np.min(lightcurve_list) - 0.05 * (1 - np.min(lightcurve_list)), 1 + 0.05 * (1 - np.min(lightcurve_list)))
+ax.set_ylim(np.min(R) - 0.05 * (1 - np.min(R)), 1 + 0.05 * (1 - np.min(R)))
 
 ax.set_xlabel(r'$\rm{Orbital \ Phase}$')
 ax.set_ylabel(r'$\Re$')
@@ -72,4 +83,4 @@ ax.tick_params(which = 'both', direction = 'in', right = True, top = True)
 
 plt.tight_layout()
 
-plt.savefig('../../' + paramsFilename + '_lightcurvePlot.pdf', dpi = 150)
+plt.savefig(PARENTPATH + '/figures/' + paramsFilename + '_lightcurvePlot.pdf', dpi = 150)

plottingScripts/plotSpectra.py
@@ -36,7 +36,7 @@ the depth in the lightcurve during full occultation
 of the planet's R_0.
 """
 
-plotMeanSpectrum = False # Plot the spectrum averaged over all orbital phases between planetary ingress and egress
+plotMeanSpectrum = True # Plot the spectrum averaged over all orbital phases between planetary ingress and egress
 plotSpectra = True  # Plot the spectra at all orbital phases
 plotBenchmarkSpectra = True # Plot barometric benchmark spectra at all orbital phases (only if the barometric benchmark option is True in the setup file)
 
@@ -48,8 +48,8 @@ with open(PARENTPATH + '/setupFiles/' + paramsFilename + '.txt') as file:
 architectureDict = param['Architecture']
 gridsDict = param['Grids']
 
-orbphase = flux.constructAxis(gridsDict, architectureDict, 'orbphase')
-wavelength = flux.constructAxis(gridsDict, architectureDict, 'wavelength') * 1e8 # In Angstrom
+orbphase = flux.constructAxis(gridsDict, architectureDict, 'orbphase') # In radians
+wavelength = flux.constructAxis(gridsDict, architectureDict, 'wavelength') # In cm
 
 R_star = architectureDict['R_star']
 R_0 = architectureDict['R_0']
@@ -83,26 +83,26 @@ Plot the spectrum and store the figure
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111)
 
-cmap = matplotlib.cm.get_cmap('Accent')
+cmap = matplotlib.cm.get_cmap('viridis')
 
 for idx, r in enumerate(R_plot):
 
     if plotSpectra:
-        ax.plot(wavelength, r, color = cmap(float(idx) / float(len(orbphase))), linewidth = 1, label = r'$\phi=$' + str(np.round(orbphase[idx], 3)))
+        ax.plot(wavelength * 1e8, r, color = cmap(float(idx) / float(len(orbphase))), linewidth = 1, label = r'$\phi=$' + str(np.round(orbphase[idx] / (2. * np.pi), 3)))
 
     if benchmark and plotBenchmarkSpectra:
 
-        ax.plot(wavelength, R_benchmark_plot[idx], color = cmap(float(idx) / float(len(orbphase))), linewidth = 1, linestyle = '--')
+        ax.plot(wavelength * 1e8, R_benchmark_plot[idx], color = cmap(float(idx) / float(len(orbphase))), linewidth = 1, linestyle = '--')
 
-if len(orbphase) > 1 and plotMeanSpectrum:
+orbphaseFullIngress = np.arcsin((R_star - R_0) / a_p) # Orbital phase at which the planet's R_0 is fully within the stellar disk
+SEL = np.abs(orbphase) <= orbphaseFullIngress
 
-    orbphaseFullIngress = np.arcsin((R_star - R_0) / a_p) / (2. * np.pi) # Orbital phase at which the planet's R_0 is fully within the stellar disk
+if len(orbphase[SEL]) > 1 and plotMeanSpectrum:
 
-    SEL = np.abs(orbphase) <= orbphaseFullIngress
     R_avg = np.mean(R[:, SEL], axis = 1)
     R_avg_norm = R_avg + (1 - np.max(R_avg))
 
-    ax.plot(wavelength, R_avg_norm, color = 'black', linewidth = 2, label = 'Mean')
+    ax.plot(wavelength * 1e8, R_avg_norm, color = 'black', linewidth = 2, label = 'Mean')
 
 
 
@@ -117,7 +117,7 @@ ax.set_ylabel(r'$\Re$')
 ax.minorticks_on()
 ax.tick_params(which = 'both', direction = 'in', right = True, top = True)
 
-ax.set_xlim(np.min(wavelength), np.max(wavelength))
+ax.set_xlim(np.min(wavelength) * 1e8, np.max(wavelength) * 1e8)
 ax.set_ylim(np.min(R_plot) - 0.05 * (1 - np.min(R_plot)), 1 + 0.05 * (1 - np.min(R_plot)))
 
 plt.tight_layout()

plottingScripts/plotSpectralField.py
@@ -10,6 +10,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 import json
 import sys
+import os
+SCRIPTPATH = os.path.realpath(__file__)
+GITPATH = os.path.dirname(os.path.dirname(SCRIPTPATH))
+PARENTPATH = os.path.dirname(GITPATH)
+sys.path.append(GITPATH)
+import eliteScripts.fluxDecrease as flux
 
 matplotlib.rcParams['axes.linewidth'] = 2.5
 matplotlib.rcParams['xtick.major.size'] = 10
@@ -29,27 +35,21 @@ Read in settings file and stored light curve
 
 paramsFilename = sys.argv[1]
 
-with open('../../' + paramsFilename + '.txt') as file:
+with open(PARENTPATH + '/setupFiles/' + paramsFilename + '.txt') as file:
     param = json.load(file)
 
-grids_dict = param['Grids']
+architectureDict = param['Architecture']
+gridsDict = param['Grids']
 
-wavelength = (np.arange(grids_dict['lower_w'], grids_dict['upper_w'] + grids_dict['resolution'], grids_dict['resolution']) - grids_dict['resolution']  / 2.) * 1e8 # In Angstrom
+wavelength_axis = flux.constructAxis(gridsDict, architectureDict, 'wavelength')
+orbphase_axis = flux.constructAxis(gridsDict, architectureDict, 'orbphase')
 
-LightcurveFile = np.loadtxt('../../' + paramsFilename + '_lightcurve.txt')
+wavelength, orbphase = np.meshgrid(wavelength_axis, orbphase_axis, indexing = 'ij')
 
-orbphase_border = grids_dict['orbphase_border']
-orbphase_steps = grids_dict['orbphase_steps']
-orbphase = np.linspace(-orbphase_border, orbphase_border + 2 * orbphase_border / (float(orbphase_steps) - 1.), 1 + int(orbphase_steps)) - orbphase_border / (float(orbphase_steps) - 1.)
+LightcurveFile = np.loadtxt(PARENTPATH + '/output/' + paramsFilename + '_lightcurve.txt')
 
-wavelength_grid, orbphase_grid = np.meshgrid(wavelength, orbphase)
 
-lightcurve_list = []
-
-for idx in range(len(wavelength) - 1):
-    lightcurve_list.append(LightcurveFile[:, idx + 1])
-
-lightcurves = np.array(lightcurve_list)
+R = LightcurveFile[:, 2].reshape(len(wavelength_axis), len(orbphase_axis))
 
 """
 Plot the light curve and store the figure
@@ -60,11 +60,11 @@ ax = fig.add_subplot(111)
 
 cmap = matplotlib.cm.get_cmap('Accent')
 
-im = ax.pcolormesh(wavelength_grid, orbphase_grid / 2 * np.pi, lightcurves.T, cmap = 'Spectral')
+im = ax.pcolormesh(wavelength * 1e8, orbphase / 2 * np.pi, R, cmap = 'Spectral')
 
 cbar = fig.colorbar(im, ax = ax)
 cbar.set_label(r'$\Re$')
-
+cbar.ax.minorticks_on()
 
 ax.set_xlabel(r'$\lambda\,[\AA]$')
 ax.set_ylabel(r'$\rm{Orbital \ Phase}$')
@@ -75,4 +75,4 @@ ax.tick_params(which = 'both', direction = 'in', right = True, top = True)
 
 plt.tight_layout()
 
-plt.savefig('../../' + paramsFilename + '_spectralFieldPlot.pdf', dpi = 150)
+plt.savefig(PARENTPATH + '/figures/' + paramsFilename + '_spectralFieldPlot.pdf', dpi = 150)

plottingScripts/plotStellarSpectrum.py
@@ -17,6 +17,7 @@ PARENTPATH = os.path.dirname(GITPATH)
 sys.path.append(GITPATH) 
 import eliteScripts.gasProperties as gasprop
 import eliteScripts.stellarSpectrum as stellar
+import eliteScripts.fluxDecrease as flux
 
 
 matplotlib.rcParams['axes.linewidth'] = 2.5
@@ -49,7 +50,7 @@ R_star = architectureDict['R_star']
 T_starrot = architectureDict['period_starrot']
 
 
-wavelength = np.arange(gridsDict['lower_w'], gridsDict['upper_w'], gridsDict['resolution'])
+wavelength = flux.constructAxis(gridsDict, architectureDict, 'wavelength')
 
 T_eff = architectureDict['T_eff']
 log_g = architectureDict['log_g']